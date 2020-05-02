b96c0c63d5ba5fe6178feadb6cc2624527e80b81
Mayur Goyal <Mayurg6832.mg@gmail.com>
2020-05-02 16:53:04 +0530

    Add files via upload

---
+++ Merge_Sort.py
@@ -0,0 +1,33 @@
+def merge(arr):
+    if len(arr)>1:
+        mid=len(arr)//2
+        l=arr[:mid]
+        r=arr[mid:]
+
+        merge(l)
+        merge(r)
+    
+        i=j=k=0
+        while i<len(l) and j<len(r):
+            if l[i]<r[j]:
+                arr[k]=l[i]
+                i+=1
+            else:
+                arr[k]=r[j]
+                j+=1
+            k+=1
+        print("l",l)
+        print('r',r)
+        print('arr',arr)
+        while i<len(l):
+            arr[k]=l[i]
+            i+=1
+            k+=1
+        while j<len(r):
+            arr[k]=r[j]
+            j+=1
+            k+=1
+
+arr=[4,3,6,1,8,7,2]
+merge(arr)
+print(arr)
